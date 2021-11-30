861507202cd84947a203b4cf52b19de11e509af4
eirinipr <93040250+eirinipr@users.noreply.github.com>
2021-11-30 23:21:53 +0200

    mvc created

---
+++ FundProjectMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FundProjectMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
