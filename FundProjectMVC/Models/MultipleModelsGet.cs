3433dbe552ec5d9736ffd9f9e3dd05a5cc36aa79
eirinipr <93040250+eirinipr@users.noreply.github.com>
2021-12-07 17:43:49 +0200

    multiplemodelsget - create reward

---
+++ FundProjectMVC/Models/MultipleModelsGet.cs
@@ -0,0 +1,14 @@
+﻿using FundProjectAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FundProjectMVC.Models
+{
+    public class MultipleModelsGet
+    {
+        public ProjectDto ProjectDto { get; set; }
+        public RewardPackageDto RewardPackageDto { get; set; }
+    }
+}
